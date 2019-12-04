d163b6eed48a59afa44435bc49ab6442b398e1e3
KhanhNCK <khanhcong696@gmail.com>
2019-12-05 03:37:38 +0700

    khanh29 push2

---
+++ public/js/home.js
@@ -0,0 +1,7 @@
+//header
+function openNav() {
+  document.getElementById("menu_mobile").style.width = "250px";
+}
+function closeNav() {
+  document.getElementById("menu_mobile").style.width = "0";
+}
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -4,13 +4,303 @@ Trang chủ
 @endsection
 
 @section('css')
-css riêng
+
 @endsection
 
 @section('js')
-js riêng
+
 @endsection
 
 @section('content')
-đây là trang chủ
+<main>
+    <div class="product1">
+        <div class="cap-main">
+            <h3>Talent Wins</h3>
+        </div>
+        <div class="row line">
+            <div class="col-md-1 "></div>
+            <div class="col-md-2 col-sm-4 item">
+                <a href="#">
+                    <img src="images/Check in and Check out-01.png" alt="">
+                    <div class="title">
+                        <p>Star</p>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-2 col-sm-4 item">
+                <a href="#">
+                    <img src="images/Health-01.png" alt="">
+                    <div class="title">
+                        <p>Health</p>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-2 col-sm-4 item">
+                <a href="#">
+                    <img src="images/Diary.png" alt="">
+                    <div class="title">
+                        <p>Photos</p>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-2 col-sm-4 item">
+                <a href="#">
+                    <img src="images/Check in and Check out-01.png" alt="">
+                    <div class="title">
+                        <p>Star</p>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-2 col-sm-4 item">
+                <a href="#">
+                    <img src="images/Check in and Check out-01.png" alt="">
+                    <div class="title">
+                        <p>Star</p>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-1 "></div>
+
+        </div>
+        <div class="row line">
+            <div class="col-md-1 "></div>
+            <div class="col-md-2 col-sm-4 item">
+                <a href="#">
+                    <img src="images/Check in and Check out-01.png" alt="">
+                    <div class="title">
+                        <p>Star</p>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-2 col-sm-4 item">
+                <a href="#">
+                    <img src="images/Health-01.png" alt="">
+                    <div class="title">
+                        <p>Health</p>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-2 col-sm-4 item">
+                <a href="#">
+                    <img src="images/Diary.png" alt="">
+                    <div class="title">
+                        <p>Photos</p>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-2 col-sm-4 item">
+                <a href="#">
+                    <img src="images/Check in and Check out-01.png" alt="">
+                    <div class="title">
+                        <p>Star</p>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-2 col-sm-4 item">
+                <a href="#">
+                    <img src="images/Check in and Check out-01.png" alt="">
+                    <div class="title">
+                        <p>Star</p>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-1 "></div>
+
+        </div>
+        <div class="row line">
+            <div class="col-md-1 "></div>
+            <div class="col-md-2 col-sm-4 item">
+                <a href="#">
+                    <img src="images/Check in and Check out-01.png" alt="">
+                    <div class="title">
+                        <p>Star</p>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-2 col-sm-4 item">
+                <a href="#">
+                    <img src="images/Health-01.png" alt="">
+                    <div class="title">
+                        <p>Health</p>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-2 col-sm-4 item">
+                <a href="#">
+                    <img src="images/Diary.png" alt="">
+                    <div class="title">
+                        <p>Photos</p>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-2 col-sm-4 item">
+                <a href="#">
+                    <img src="images/Check in and Check out-01.png" alt="">
+                    <div class="title">
+                        <p>Star</p>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-2 col-sm-4 item">
+                <a href="#">
+                    <img src="images/Check in and Check out-01.png" alt="">
+                    <div class="title">
+                        <p>Star</p>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-1 "></div>
+
+        </div>
+    </div>
+    <div class="product2">
+        <div class="cap-main">
+            <h3>Profiles</h3>
+        </div>
+        <div class="row line">
+            <div class="col-md-1 "></div>
+            <div class="col-md-2 col-sm-4 item">
+                <a href="#">
+                    <img src="images/Check in and Check out-01.png" alt="">
+                    <div class="title">
+                        <p>Star</p>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-2 col-sm-4 item">
+                <a href="#">
+                    <img src="images/Health-01.png" alt="">
+                    <div class="title">
+                        <p>Health</p>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-2 col-sm-4 item">
+                <a href="#">
+                    <img src="images/Diary.png" alt="">
+                    <div class="title">
+                        <p>Photos</p>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-2 col-sm-4 item">
+                <a href="#">
+                    <img src="images/Check in and Check out-01.png" alt="">
+                    <div class="title">
+                        <p>Star</p>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-2 col-sm-4 item">
+                <a href="#">
+                    <img src="images/Check in and Check out-01.png" alt="">
+                    <div class="title">
+                        <p>Star</p>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-1 "></div>
+
+        </div>
+    </div>
+    <div class="product3">
+        <div class="cap-main">
+            <h3>Setup & Settings</h3>
+        </div>
+        <div class="row line">
+            <div class="col-md-1 "></div>
+            <div class="col-md-2 col-sm-4 item">
+                <a href="#">
+                    <img src="images/Check in and Check out-01.png" alt="">
+                    <div class="title">
+                        <p>Star</p>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-2 col-sm-4 item">
+                <a href="#">
+                    <img src="images/Health-01.png" alt="">
+                    <div class="title">
+                        <p>Health</p>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-2 col-sm-4 item">
+                <a href="#">
+                    <img src="images/Diary.png" alt="">
+                    <div class="title">
+                        <p>Photos</p>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-2 col-sm-4 item">
+                <a href="#">
+                    <img src="images/Check in and Check out-01.png" alt="">
+                    <div class="title">
+                        <p>Star</p>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-2 col-sm-4 item">
+                <a href="#">
+                    <img src="images/Check in and Check out-01.png" alt="">
+                    <div class="title">
+                        <p>Star</p>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-1 "></div>
+
+        </div>
+    </div>
+    <div class="product4">
+        <div class="cap-main">
+            <h3>Help</h3>
+        </div>
+        <div class="row line">
+            <div class="col-md-1 "></div>
+            <div class="col-md-2 col-sm-4 item">
+                <a href="#">
+                    <img src="images/Check in and Check out-01.png" alt="">
+                    <div class="title">
+                        <p>Star</p>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-2 col-sm-4 item">
+                <a href="#">
+                    <img src="images/Health-01.png" alt="">
+                    <div class="title">
+                        <p>Health</p>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-2 col-sm-4 item">
+                <a href="#">
+                    <img src="images/Diary.png" alt="">
+                    <div class="title">
+                        <p>Photos</p>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-2 col-sm-4 item">
+                <a href="#">
+                    <img src="images/Check in and Check out-01.png" alt="">
+                    <div class="title">
+                        <p>Star</p>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-2 col-sm-4 item">
+                <a href="#">
+                    <img src="images/Check in and Check out-01.png" alt="">
+                    <div class="title">
+                        <p>Star</p>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-1 "></div>
+        </div>
+    </div>
+</main>
 @endsection
