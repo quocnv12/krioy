98a5a4a5ac2a14a04f743a5c26fef6501adfe536
vanquocnb@gmail.com <vanquocnb@gmail.com>
2019-12-13 11:59:19 +0700

    up

--- routes/web.php
+++ routes/web.php
@@ -19,13 +19,54 @@
 //     	return view('admin.home');
 // 	})->name('home');
 // });
+
+
 Route::get('/', function () {
     return view('pages.home');
 })->name('home');
 
+Route::group(['prefix' => 'kids-now'], function () {
+    //---------------children----------------
+    Route::group(['prefix' => 'children'], function () {
+        Route::get('profiles', function () {
+            return view('pages.children.child_profile');
+        });
+        Route::get('create', function () {
+            return view('pages.children.create_child');
+        });
+        Route::get('edit', function () {
+            return view('pages.children.edit_child');
+        });
+    });
 
-//test
-Route::get('children/profile', function (){
-   return view('pages.children.child_profile');
+       //---------------staff----------------
+       Route::group(['prefix' => 'staff'], function () {
+            Route::get('profiles', function () {
+                return view('pages.staff.staff_profile');
+            });
+            Route::get('create_staff', function () {
+                return view('pages.staff.create_staff');
+            });
+            Route::get('edit_staff', function () {
+                return view('pages.staff.edit_staff');
+            });
+    });
+
+
+     //---------------attendance----------------
+        Route::group(['prefix' => 'attendance'], function () {
+            Route::get('', function () {
+                return view('pages.attendance.attendance');
+            });
+            
+            
+    });
+
+
+
+    
+
+
 });
+
 
