2e03a0e8e260ef753004814e3f0e8a667c6fd388
nguyentribao1998 <44550441+nguyentribao1998@users.noreply.github.com>
2020-01-15 20:51:14 +0700

    add

--- resources/views/pages/observation/observation.blade.php
+++ resources/views/pages/observation/observation.blade.php
@@ -56,7 +56,7 @@
 					<div class="comment">
 						<div class="row">
 							<div class="col-md-11 input_box">
-								<span>Enter Details here *</span>
+								<span>Enter Detail here *</span>
 								<input type="text" name="detailObservation" placeholder="Enter Details here *">
 							</div>
 							<div class="col-md-1">
