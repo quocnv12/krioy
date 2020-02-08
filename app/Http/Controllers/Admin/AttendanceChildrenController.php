0410104bebbdf20b096cc2b968c1c4a96e02112e
Pham Phuong <it.pdphuong@gmail.com>
2020-02-08 10:31:50 +0700

    attendance

--- app/Http/Controllers/Admin/AttendanceChildrenController.php
+++ app/Http/Controllers/Admin/AttendanceChildrenController.php
@@ -29,19 +29,24 @@ class AttendanceChildrenController extends Controller
             ->get();
         $day = Carbon::now('Asia/Ho_Chi_Minh')->toTimeString();
         $dayupdate =  date('d', strtotime($day));
+        $data['time'] = date('H:s:i', strtotime($day));
 
-        $data['count_chil'] = $data['children_profiles']->count();
-        $data['count_in'] = Children_status::where('status','1')->whereDay('updated_at',$dayupdate)->count();
-        $data['count_out'] = Children_status::where('status','2')->whereDay('updated_at',$dayupdate)->count();
-        $data['count_absent'] = Children_status::where('status','3')->whereDay('updated_at',$dayupdate)->count();
         
+        $data['id'] = $id;
+        $data['count_chil'] = $data['children_profiles']->count();
+        $data['count_in'] = Children_status::where('status','1')->where('id_program',$id)->whereDay('updated_at',$dayupdate)->count();
+        $data['count_out'] = Children_status::where('status','2')->where('id_program',$id)->whereDay('updated_at',$dayupdate)->count();
+        $data['count_absent'] = Children_status::where('status','3')->where('id_program',$id)->whereDay('updated_at',$dayupdate)->count();
+        $data['count_active'] = Children_status::where('active','1')->where('id_program',$id)->whereDay('updated_at',$dayupdate)->count();
+        
+
         return view('pages.attendance.attendance',$data);
     }
    
-    public function postAdd(Request $req){
+    public function postAdd(Request $req,$id){
         
         $children_attendance = explode(',', $req->children_attendance);
-        
+                
         $day = Carbon::now('Asia/Ho_Chi_Minh')->toTimeString();
         $monthupdate =  date('m', strtotime($day));
         $dayupdate =  date('d', strtotime($day));
@@ -49,19 +54,23 @@ class AttendanceChildrenController extends Controller
         $timestatus =  date('d-m-Y H:i:s', strtotime($day));
 
         
-        foreach ($children_attendance as $id_children) {
-            // check attendance / day,month,year
-            $check_attendance_month = Attendance_children::where('id_children',$id_children)->whereMonth('created_at',$monthupdate)->first();
-            $check_attendance_day = Attendance_children::where('id_children',$id_children)->whereDay('updated_at',$dayupdate)->first();
-            $check_attendance_year = Attendance_children::where('id_children',$id_children)->whereYear('updated_at',$yearupdate)->first();
-            
-            if(empty($check_attendance_day)){
-                echo 'Chưa điểm rank';
-                if(empty($check_attendance_year)){
-                    echo ' Không trùng năm ';
+        if($req->children_attendance==null){
+            return redirect()->back()->with('danger','Please choose children !')->withInput();
+        }else{
+            foreach ($children_attendance as $id_children) {
 
+                $chil_profiles = ChildrenProfiles::find($id_children);
+                // check attendance / day,month,year
+                $check_attendance_month = Children_status::where('id_children',$id_children)->whereMonth('created_at',$monthupdate)->first();
+                $check_attendance_year = Children_status::where('id_children',$id_children)->whereYear('updated_at',$yearupdate)->first();
+
+                $check_attendance_day = Children_status::where('id_children',$id_children)->whereDay('updated_at',$dayupdate)->first();
+
+                if(empty($check_attendance_day)){
+                    echo ' Chưa điểm rank ';
                     $chil_status = new Children_status;
                     $chil_status->id_children = $id_children;
+                    $chil_status->id_program = $id;
                     if($req->children_status == 1) {
                         $chil_status->in = $timestatus;
                     }elseif($req->children_status == 2){
@@ -70,84 +79,33 @@ class AttendanceChildrenController extends Controller
                         $chil_status->absent = $timestatus;
                     }
                     $chil_status->status = $req->children_status;
+                    $chil_status->active = '1';
                     $chil_status->save();
-
-                    $atd = new Attendance_children;
-                    $atd->id_children = $id_children;
-                    if($req->children_status == 1) {
-                        $atd->total_come += 1;
-                        $atd->total_absent += 0;
-                    }else{
-                        $atd->total_come += 0;
-                        $atd->total_absent += 1;
-                    }
-                    $atd->save();
-                }else{
-                    if(empty($check_attendance_month)){
-                        $atd = new Attendance_children;
-                        $atd->id_children = $id_children;
-                        if($req->children_status == 1) {
-                            $atd->total_come += 1;
-                            $atd->total_absent += 0;
-                        }else{
-                            $atd->total_come += 0;
-                            $atd->total_absent += 1;
-                        }
-                        $atd->save();
-                        
-                    }else{
-                        $atd = Attendance_children::where('id_children','=',$id_children)->whereMonth('created_at',$monthupdate)->first();
-                        if($req->children_status == 1) {
-                            $atd->total_come += 1;
-                        }else{
-                            $atd->total_absent += 1;
-                        }
-                        $atd->save();
-                    }
-                }
-
-                
-            }else{
-                echo 'Đã điểm rank rồi !';
-                if(empty($check_attendance_month)){
-                    $atd = new Attendance_children;
-                    $atd->id_children = $id_children;
-                    if($req->children_status == 1) {
-                        $atd->total_come += 1;
-                        $atd->total_absent += 0;
-                    }else{
-                        $atd->total_come += 0;
-                        $atd->total_absent += 1;
-                    }
-                    $atd->save();
                     
                 }else{
-                // update children_status
+                    echo ' Đã điểm rank ';
                     $chil_status = Children_status::where('id_children',$id_children)->whereDay('updated_at',$dayupdate)->first();
                     if($req->children_status == 1) {
                         $chil_status->in = $timestatus;
+                        $chil_status->absent = null;
                     }elseif($req->children_status == 2){
                         $chil_status->out = $timestatus;
+                        $chil_status->absent = null;
                     }elseif($req->children_status == 3){
+                        $chil_status->in = null;
+                        $chil_status->out = null;
                         $chil_status->absent = $timestatus;
                     }
                     $chil_status->status = $req->children_status;
-
-                // update Attendance_children
-                    $atd = Attendance_children::where('id_children','=',$id_children)->whereMonth('created_at',$monthupdate)->first();
-                    if($req->children_status == 1) {
-                        $atd->total_come += 1;
-                        $atd->total_absent -= 1;
-                    }else{
-                        $atd->total_come -= 1;
-                        $atd->total_absent += 1;
-                    }
-                    $atd->save();
+                    $chil_status->active = '1';
+                    $chil_status->save();
                 }
+                
             }
-         
+            return redirect()->back()->with('success','Attendance success !')->withInput();
         }
         
+
         
     }
 }
--- database/migrations/2020_02_07_112619_create_children_status_table.php
+++ database/migrations/2020_02_07_112619_create_children_status_table.php
@@ -15,12 +15,15 @@ class CreateChildrenStatusTable extends Migration
     {
         Schema::create('children_status', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->bigInteger('id_children')->unsigned();
+            $table->bigInteger('id_program')->unsigned();
             $table->string('in')->nullable();
             $table->string('out')->nullable();
             $table->string('absent')->nullable();
             $table->tinyInteger('status')->nullable();
-            $table->bigInteger('id_children')->unsigned;
+            $table->tinyInteger('active')->nullable();
             $table->foreign('id_children')->references('id')->on('children_profiles')->onDelete('cascade');
+            $table->foreign('id_program')->references('id')->on('programs')->onDelete('cascade');
             $table->timestamps();
         });
     }
