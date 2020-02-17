6ce34ff8d88d360628f723fe7c544e0d67b98915
caominhduc <caominhduc1999@gmail.com>
2020-02-17 11:16:43 +0700

    excel

--- app/Http/Controllers/Admin/HealthController.php
+++ app/Http/Controllers/Admin/HealthController.php
@@ -13,6 +13,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
+use Maatwebsite\Excel\Facades\Excel;
 
 class HealthController extends Controller
 {
@@ -23,6 +24,13 @@ class HealthController extends Controller
         return view('pages.heath.list', compact('health','programs'));
     }
 
+    public function view($id){
+        $health = HealthModel::find($id);
+
+        $children_profiles = ChildrenProfiles::where('id',$health->id_children)->first();
+        return view('pages.heath.view', compact('health','children_profiles'));
+    }
+
     public function getAdd(){
         $health = HealthModel::all();
         $programs = Programs::all();
@@ -30,111 +38,142 @@ class HealthController extends Controller
     }
     public function postAdd(Request $request)
     {
+        $this->validate($request,
+            [
+                'children_health'   =>  'required',
+                'sick'  =>  'nullable',
+                'medicine'  =>  'nullable',
+                'growth'  =>  'nullable',
+                'incident'  =>  'nullable',
+                'blood_group'  =>  'nullable',
+            ],
+            [
+                'children_health.required'  =>  'Please choose children',
+            ]);
 
-        if($request->sick==null)
-        {
-            return redirect()->back()->with('thongbao1','Pleasea choose sick !')->withInput();
-        }
-        elseif ($request->medicine==null) {
-            return redirect()->back()->with('thongbao2','Pleasea choose  medicine !')->withInput();
-        }
-        elseif($request->growth_height==null)
-        {
-            return redirect()->back()->with('thongbao3','Pleasea choose growth_height !')->withInput();
-        }
-        elseif($request->incident==null)
-        {
-            return redirect()->back()->with('thongbao4','Pleasea choose incident !')->withInput();
-        }
-        elseif($request->blood_group==null)
-        {
-            return redirect()->back()->with('thongbao5','Pleasea choose blood_group !')->withInput();
-        }
-        else {
-
+        $array_children = explode(',',$request->children_health);
+        foreach($array_children as $children){
             $health = new HealthModel();
 
-            $health->id_children = $request->children_health;
+            $health->id_children = $children;
             $health->sick = $request->sick;
             $health->medicine = $request->medicine;
+            $health->growth = $request->growth;
             $health->growth_height = $request->growth_height;
             $health->growth_weight = $request->growth_weight;
             $health->incident = $request->incident;
             $health->blood_group = $request->blood_group;
 
-
-
-            if ($request->hasFile('image')) {
-                $file = $request->image;
-                $filename = Str::random(9) . '.' . $file->getClientOriginalExtension();
-                $file->move(public_path('images/health/'), $filename);
-                $health->image = 'images/health/' . $filename;
+            if ($request->hasFile('clip_board')){
+                $array_file = [];
+                foreach ($request->file('clip_board') as $file_name){
+                    $uniqueFileName = (Str::random(4).'_'.$file_name->getClientOriginalName());
+                    array_push($array_file, $uniqueFileName);
+                    $file_name->move(storage_path('app/public/clip_board/') , $uniqueFileName);
+                }
+                $health->clip_board = implode('/*endfile*/',$array_file);
             }
 
             $health->save();
-
-            return redirect()->back()->with('notify', 'Add successfully');
-
         }
+
+
+
+
+
+        return redirect()->back()->with('success', 'Added children\'s health');
     }
 
     public function getEdit($id){
         $health = HealthModel::find($id);
 
-        $children = DB::table('children_profiles')->where('id',$id)->first();
-        return view('pages.heath.edit', compact('health','children'));
+        $children_profiles = ChildrenProfiles::where('id',$health->id_children)->first();
+        return view('pages.heath.edit', compact('health','children_profiles'));
     }
     public function postEdit(Request $request, $id)
     {
+        $this->validate($request,
+            [
+                'sick'  =>  'nullable',
+                'medicine'  =>  'nullable',
+                'growth'  =>  'nullable',
+                'incident'  =>  'nullable',
+                'blood_group'  =>  'nullable',
+            ]);
 
-        if ($request->sick == null) {
-            return redirect()->back()->with('thongbao1', 'Pleasea choose sick !')->withInput();
-        } elseif ($request->medicine == null) {
-            return redirect()->back()->with('thongbao2', 'Pleasea choose  medicine !')->withInput();
-        } elseif ($request->growth_height == null) {
-            return redirect()->back()->with('thongbao3', 'Pleasea choose growth_height !')->withInput();
-        } elseif ($request->incident == null) {
-            return redirect()->back()->with('thongbao4', 'Pleasea choose incident !')->withInput();
-        } elseif ($request->blood_group == null) {
-            return redirect()->back()->with('thongbao5', 'Pleasea choose blood_group !')->withInput();
-        } else {
+        $health = HealthModel::find($id);
 
-            $health = HealthModel::find($id);
+        $health->sick = $request->sick;
+        $health->medicine = $request->medicine;
+        $health->growth = $request->growth;
+        $health->growth_height = $request->growth_height;
+        $health->growth_weight = $request->growth_weight;
+        $health->incident = $request->incident;
+        $health->blood_group = $request->blood_group;
 
-            $health->sick = $request->sick;
-            $health->medicine = $request->medicine;
-            $health->growth_height = $request->growth_height;
-            $health->growth_weight = $request->growth_weight;
-            $health->incident = $request->incident;
-            $health->blood_group = $request->blood_group;
-
-
-            if ($request->image != null) {
-                // xoa anh cu
-                if ($health->image) {
-                    $old_image = $health->image;
-                    unlink($old_image);
-                }
-                $file = $request->image;
-                $filename = Str::random(9) . '.' . $file->getClientOriginalExtension();
-                $file->move(public_path('images/health/'), $filename);
-                $health->image = 'images/health/' . $filename;
-                $health->save();
+        if ($request->hasFile('clip_board')){
+            $old_array = explode('/*endfile*/',$health->clip_board);
+            foreach ($request->file('clip_board') as $file_name){
+                $uniqueFileName = (Str::random(4).'_'.$file_name->getClientOriginalName());
+                array_push($old_array, $uniqueFileName);
+                $file_name->move(storage_path('app/public/clip_board/') , $uniqueFileName);
             }
-
-            $health->save();
-
-
-            return redirect()->back()->with('notify', 'Edit successfully');
+            $health->clip_board = implode('/*endfile*/',$old_array);
         }
+
+        $health->save();
+
+        return redirect()->back()->with('success', 'Updated children\'s health');
+
     }
 
     public function getDelete($id){
-        $health= DB::table('health')->where('id',$id)->delete();
-        return redirect()->back()->with('notify','Deleted file successfully');
-    }
-    public function getSearch(Request $req){
+        $health= HealthModel::find($id);
+        if (!$health){
+            return view('pages.not-found.notfound');
+        }
 
+        if ($health->clip_board){
+            $old_array = explode('/*endfile*/',$health->clip_board);
+            foreach ($old_array as $key=>$value){
+                $file_path = 'app/public/clip_board/'.$value;
+                if ($file_path != 'app/public/clip_board/'){
+                    unlink(storage_path($file_path));
+                }
+            }
+        }
+
+        $health->delete();
+        return redirect()->back()->with('success','Deleted children\'s health');
+    }
+
+    public function displayClipboard($id,$name)
+    {
+        return response()->file(storage_path('app/public/clip_board/'.$name),[
+            'Content-Disposition' => 'inline; filename="'. $name .'"']);
+    }
+
+    public function deleteClipboard($id,$name)
+    {
+        $health = HealthModel::find($id);
+        //chuoi cu
+        $old_array = explode('/*endfile*/',$health->clip_board);
+
+        $index = array_search($name, $old_array);
+        array_splice($old_array, $index, 1);
+
+        //cap nhat lai chuoi
+        $health->clip_board = implode('/*endfile*/',$old_array);
+        $health->save();
+
+        //xoa file
+        $file_path = storage_path('app/public/clip_board/'.$name);
+        unlink($file_path);
+
+        return redirect()->back()->with('success','Deleted File');
+    }
+
+    public function getSearch(Request $req){
         $search = DB::table('children_profiles as a')
             ->where('first_name','like','%'.$req->key.'%')
             ->orWhere('last_name','like','%'.$req->key.'%')
@@ -147,9 +186,8 @@ class HealthController extends Controller
             ->paginate(5);
         return view('pages.heath.search', compact('search'));
     }
+
     public function postSearch(Request $req){
-
-
         $search = DB::table('children_profiles as a')
             ->where('first_name','like','%'.$req->key.'%')
             ->orWhere('last_name','like','%'.$req->key.'%')
@@ -159,8 +197,6 @@ class HealthController extends Controller
             ->orWhere('incident','like','%'.$req->key.'%')
             ->join('health as b', 'a.id','=','b.id_children')
             ->select('a.*','b.*')
-
-
             ->paginate(5);
         return view('pages.heath.search', compact('search'));
 
@@ -173,48 +209,61 @@ class HealthController extends Controller
         return response()->json($children_profiles);
     }
 
-    public function addSelectChild(Request $request)
-    {
-        if ($request->ajax()) {
-            $output = '';
-            $children_profiles = ChildrenProfiles::find($request->id_children);
-
-            if ($children_profiles){
-                $output = '
-                            <div class="col-lg-2 col-md-2 col-sm-2 col-xs-6 ng-star-inserted select-child-img select-child-img1" style="">
-							    <div class="child-class" style="height: 120px;text-align: center;">
-							        <div class="image">
-                                        <img class="img-circle" onerror="this.src=\'images/Child.png\';" style="height: 80px" width="80" src="' . $children_profiles->image . '">
-                                        <input type="hidden" value="' . $children_profiles->id . '">
-                                        <button class="btn btn-xs btn-danger" type="button" onclick="deleteChild(' . $children_profiles->id . ')">X</button>
-                                        <span class="limitText ng-star-inserted">' . $children_profiles->first_name . ' ' . $children_profiles->last_name . '</span>
-                                    </div>
-                                </div>
-                            </div>
-                            
-                            <script>
-                                $(\'.btn-danger\').click(function() {
-                                  $(this).parent(\'div\').parent(\'div\').parent(\'div\').hide();
-                                })
-                            </script>
-                            ';
-            }
-            return Response($output);
-        }
-    }
     public function showChildrenInProgram($id){
         $observationtype = ObservationTypeModel::all();
         $programs = Programs::all();
         $children_profiles = DB::table('children_profiles')
             ->join('children_programs','children_profiles.id','=','children_programs.id_children')
             ->where('children_programs.id_program','=',$id)
+            ->orderBy('children_profiles.first_name')
             ->get();
         return view('pages.heath.heath',['children_profiles'=>$children_profiles,
             'observationtype'=>$observationtype,
             'programs'=>$programs]);
     }
 
+    public function excel($id)
+    {
+        $health = HealthModel::find($id);
+        $children_data = ChildrenProfiles::where('id',$health->id_children)->first();
 
+        $health_array[] = array(
+            'Children Name' ,
+            'Birthday' ,
+            'Sick' ,
+            'Medicine',
+            'Height',
+            'Weight',
+            'Head Circumference',
+            'Growth',
+            'Incident',
+            'Blood Group'
+        );
+
+
+        $health_array[] = array(
+            'Children Name' =>  $children_data->first_name .' '. $children_data->last_name,
+            'Birthday'  =>  date('d-m-Y',strtotime($children_data->birthday)),
+            'Sick'  =>  $health->sick,
+            'Medicine'=>  $health->medicine,
+            'Height'=>  $health->growth_height,
+            'Weight'=>  $health->growth_weight,
+            'Head Circumference'=>  $health->growth_circumference,
+            'Growth'=>  $health->growth,
+            'Incident'=>  $health->incident,
+            'Blood Group'=>  $health->blood_group
+        );
+
+
+
+        Excel::create($children_data->first_name.' '.$children_data->last_name, function($excel) use ($health_array){
+            $excel->setTitle('Children Data');
+
+            $excel->sheet('Children Data', function($sheet) use ($health_array){
+                $sheet->fromArray($health_array, null, 'A1', false, false);
+            });
+        })->download('pdf');
+    }
 
 
 }
