ea50a49c54209938305bbc1329db5b8e7029dc97
Nguyễn Quốc <vanquocnb@gmail.com>
2020-03-09 16:25:44 +0700

    up

--- database/seeds/parent_note.php
+++ database/seeds/parent_note.php
@@ -13,11 +13,11 @@ class parent_note extends Seeder
     {
         DB::table('parent_note')->delete();
         DB::table('parent_note')->insert([
-            ['id' => '1', 'title' => 'Food', 'content' => 'Con tôi bị dị ứng canh cua','create_at'=>now()],
-            ['id' => '2', 'title' => 'Food', 'content' => 'Ok anh chị','create_at'=>now()],
+            ['id' => '1', 'title' => 'Food', 'content' => 'Con tôi bị dị ứng canh cua'],
+            ['id' => '2', 'title' => 'Food', 'content' => 'Ok anh chị'],
 
-            ['id' => '3', 'title' => 'Leave', 'content' => 'alo','create_at'=>now()],
-            ['id' => '4', 'title' => 'Leave', 'content' => 'blo','create_at'=>now()],
+            ['id' => '3', 'title' => 'Leave', 'content' => 'alo'],
+            ['id' => '4', 'title' => 'Leave', 'content' => 'blo'],
         ]);
     }
 }
