2dc226f70e4e6e0e351f966dbd2a5cb667f7262e
caominhduc <caominhduc1999@gmail.com>
2020-03-16 09:32:04 +0700

    update 16/3/2020

--- database/seeds/ParentNote.php
+++ database/seeds/ParentNote.php
@@ -13,10 +13,10 @@ class ParentNote extends Seeder
     {
         DB::table('parent_note')->delete();
         DB::table('parent_note')->insert([
-            ['id'=>'1','title'=>'Food','id_program'=>1,'id_children' => '1'],
-            ['id'=>'2','title'=>'Other','id_program'=>2,'id_children' => '2'],
-            ['id'=>'3','title'=>'Leave','id_program'=>1,'id_children' => '1'],
-            ['id'=>'4','title'=>'Health','id_program'=>2,'id_children' => '2'],
+            ['id'=>1,'title'=>'Food','id_program'=>1,'id_children' => 1],
+            ['id'=>2,'title'=>'Other','id_program'=>2,'id_children' => 2],
+            ['id'=>3,'title'=>'Leave','id_program'=>1,'id_children' => 1],
+            ['id'=>4,'title'=>'Health','id_program'=>2,'id_children' => 2],
         ]);
     }
 }
